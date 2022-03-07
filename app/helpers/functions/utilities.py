96fa0014748f80ec05425cab62bdbb913a149c3b
Donald KANTI <donaldkant2000@gmail.com>
2022-03-07 18:21:00 +0100

    Find solution to models metadata combining in order to support migrations auto generating

--- app/helpers/functions/utilities.py
+++ app/helpers/functions/utilities.py
@@ -1,5 +1,8 @@
 from sqlalchemy import MetaData
+from passlib.context import CryptContext
 
+
+crypto_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 def combine_metadata(*args):
     all_in_one_metadata = MetaData()
@@ -7,3 +10,10 @@ def combine_metadata(*args):
         for t in metadata.tables.values():
             t.tometadata(all_in_one_metadata)
     return all_in_one_metadata
+
+def hash_password(password: str):
+    return crypto_context.hash(password)
+
+def verify_hash(plain_password: str, hash: str):
+    return crypto_context.verify(plain_password, hash)
+
